140b2b905b6a0ca6bb9ea9f081a42e0df85fad7d
Justin <jcww33@gmail.com>
2011-06-11 12:18:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intouch::Application.config.secret_token = 'be35485563d9738314de833aea4e8c51a6044eabf313ca059131bead04dc8a75ac52eda59eca16caf78f94216f0c16db4e1664798d1c5a36db01f3c9392ebc90'
