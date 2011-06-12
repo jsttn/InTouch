972657f0b50a5df71c3df8127f0ce6b260a1c859
Justin <jcww33@gmail.com>
2011-06-11 22:51:34 -0400

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -65,6 +65,11 @@ describe UsersController do
               post :create, :user => @attr
               flash[:success].should =~ /Welcome to InTouch/i
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end 
     end
   end 
 end 
